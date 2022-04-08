6d5fc7d8f456397b0a19bd7d6c30577314225c27
tiendungmc97 <tiendungmc97@gmail.com>
2022-04-08 16:17:19 +0700

    add

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const jsonServer = require('json-server');
+const queryString = require('query-string');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
@@ -14,13 +15,33 @@ server.get('/echo', (req, res) => {
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
 server.use(jsonServer.bodyParser);
+
+router.render = (req, res) => {
+  const headers = res.getHeaders();
+  const totalCountHeader = headers['x-total-count'];
+  const queryParam = queryString.parse(req._parsedUrl.query);
+
+  console.log("adsf",queryParam)
+  if(req.method === 'GET' && totalCountHeader){
+    const result = {
+      data: res.locals.data,
+      pagination: {
+        _page: queryParam._page || 1,
+        _limit: queryParam._limit || 10,
+        _totalRows: totalCountHeader
+      }
+    }
+    return res.jsonp(result)
+  }
+  res.jsonp(res.locals.data);
+};
+
 server.use((req, res, next) => {
   if (req.method === 'POST') {
     (req.body.ctime = Date.now()), (req.body.mtime = Date.now());
   } else if (req.method === 'PATCH') {
     req.body.mtime = Date.now();
   }
-  // Continue to JSON Server router
   next();
 });
 
