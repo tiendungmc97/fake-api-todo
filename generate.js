0ff55c777fe13a555204e09d2d980d7f0c1f7d00
tiendungmc97 <tiendungmc97@gmail.com>
2022-04-12 14:07:55 +0700

    change api

--- generate.js
+++ generate.js
@@ -1,50 +1,98 @@
- const { number } = require('./helpers/random');
+const { number } = require('./helpers/random');
 const { faker } = require('@faker-js/faker');
 const fs = require('fs');
+const time = new Date();
 
-const randomProfile = (n) => {
-  if (n <= 0) return [];
-  const profileList = [];
-  Array.from(new Array(n)).forEach(() => {
-    const profile = {
-      id: number(6),
-      name: faker.name.findName(),
-      gender: faker.name.gender(),
-      job: faker.name.jobDescriptor(),
-    };
-    profileList.push(profile);
-  });
-  return profileList;
-};
-const randomTaskList = (profileList, n) => {
+const users = [
+  {
+    id: '438010',
+    inforUserID: "823984",
+    username: 'admin',
+    password: '123456',
+  },
+  {
+    id: '438013',
+    inforUserID: "823985",
+    username: 'user',
+    password: '123456',
+  },
+];
+
+const inforUsers = [
+  {
+    id: '823984',
+    firstName: 'Nguyen',
+    lastName: 'Van X',
+    phone: '0129324312',
+    address: 'Ha Noi',
+    email: 'email@gmail.com',
+    ctime: 1649745122535,
+    mtime: 1649745122535,
+  },
+  {
+    id: '823985',
+    firstName: 'Nguyen',
+    lastName: 'Tien Teo',
+    phone: '0129324312',
+    address: 'jhakdhfkd dsfdf',
+    email: 'email@gmail.com',
+    ctime: 1649745122535,
+    mtime: 1649745122535,
+  },
+];
+
+// const randomInforUsers = (n) => {
+//   if (n <= 0) return [];
+//   const inforUsers = [];
+//   Array.from(new Array(n)).forEach(() => {
+//     const user = {
+//       id: number(6),
+//       firstName: faker.name.firstName(),
+//       lastName: faker.name.lastName(),
+//       phone: faker.phone.phoneNumber(),
+//       address: faker.address.streetAddress(),
+//       email: faker.name.email(),
+//       ctime: time.now(),
+//       mtime: time.now(),
+//     };
+//     inforUsers.push(user);
+//   });
+//   return inforUsers;
+// };
+
+const randomTodos = (users, n) => {
   if (n <= 0) return;
-  const taskList = [];
-  for (const profile of profileList) {
+  const todos = [];
+  for (const user of users) {
     Array.from(new Array(n)).forEach(() => {
-      const task = {
+      const todo = {
         id: number(6),
-        profileId: profile.id,
-        title: faker.animal.bird(),
+        userId: user.id,
+        task: faker.lorem.words(),
+        comment: faker.lorem.text(),
+        timeTo: Date.now(),
+        timeFrom: Date.now()+3234234,
         ctime: Date.now(),
         mtime: Date.now(),
       };
-      taskList.push(task);
+      todos.push(todo);
     });
   }
-  return taskList;
+  return todos;
 };
 
 /// IIFE - Immediately invoked function expressions
 (() => {
   // random data
-  const profile = randomProfile(5);
-  const taskList = randomTaskList(profile, 20);
+
+  // const inforUsers = randomInforUsers(3);
+  const todos = randomTodos(users, 20);
 
   // prepare data
   const db = {
-    posts: taskList,
-    comments: [],
-    profile: profile,
+    todos: todos,
+    users: users,
+    inforUsers: inforUsers,
   };
 
   // write db object to db.json
